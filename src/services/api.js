1d4ab18d10c532a09de6b0cef0eb8a06b0440ba7
Adelino Masioli <alfjuniorbh.web@gmail.com>
2020-04-19 14:08:45 +0100

    React Native Concepts Challenge

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: 'http://localhost:3333',
 });
 
 export default api;
+
+/**
+ * adb kill-server
+ * localhost: adb reverse tcp:3333 tcp:3333 */
